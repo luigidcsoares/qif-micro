147668fd28662902762913098e5343b7658285a5
Luigi D. C. Soares <dev@luigidcsoares.com>
2025-08-29 07:24:06 +1000

    chore: preparing repository for python library

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.pyright
+              pkgs.python313
+              pkgs.uv
+            ];
+          };
+        };
+    };
+}
