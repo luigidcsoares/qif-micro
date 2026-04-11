391c4af517100a8aba29e1aa66974eb2e5a3ca11
Luigi D. C. Soares <dev@luigidcsoares.com>
2026-04-11 12:22:26 -0300

    chore: deleted files

--- src/qif_micro/mechanism/__init__.py
+++
@@ -1,5 +0,0 @@
-from .geometric import build as geometric
-from .random_response import build as random_response
-from .record import build as record
-
-__all__ = ["geometric", "random_response", "record"]
